4b7f3620456a41f11829fa28d2fd76f8065a7cc4
Zakaria Bekkar <zakaria.bekkar13@gmail.com>
2020-12-13 20:34:40 +0100

    server mod

--- app/server.py
+++ app/server.py
@@ -33,7 +33,7 @@ async def download_file(url, dest):
 async def setup_learner():
     #await download_file(export_file_url, path / export_file_name)
     try:
-        learn = load_learner('app/models/model_1_test_94.pkl')
+        learn = load_learner(file='models/model_1_test_94.pkl')
         return learn
     except :
         print('An exception occured')
